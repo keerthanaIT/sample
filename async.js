35778e583e6e549f4bca7620c16d2fd1b33946f3
keerthanaIT <keerthanam119@gmail.com>
2018-05-29 20:11:48 +0530

    async

---
+++ async.js
@@ -0,0 +1,10 @@
+console.log('app start');
+setTimeout(() => {
+console.log('data added after 3 sec');
+
+},2000);
+setTimeout(() => {
+    console.log('second timeout');
+},0);
+console.log('app end');
+
